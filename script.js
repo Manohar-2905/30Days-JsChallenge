2adeccd49cecbbb854ba6efe34046848c192f7fa
Manohar-2905 <kumarmanohar6206@gmail.com>
2025-04-02 19:34:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ window.addEventListener('keydown',function(event){
         key.classList.remove('playing')
     },100)
 });
+
+
+document.querySelectorAll(".key").forEach(key => {
+    key.addEventListener("touchstart", function () {
+        const keyCode = this.getAttribute("data-key");
+        const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+        if (!audio) return;
+
+        audio.currentTime = 0;
+        audio.play();
+
+        this.classList.add("playing");
+        setTimeout(() => this.classList.remove("playing"), 100);
+    });
+});
